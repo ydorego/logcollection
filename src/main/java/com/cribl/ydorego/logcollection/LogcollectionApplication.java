d32fc5ba5a4f182f1c8ef5d243e97cc3af1ed020
Yves do Rego <ydorego@gmail.com>
2022-04-23 17:56:47 -0400

    Initial commit

---
+++ src/main/java/com/cribl/ydorego/logcollection/LogcollectionApplication.java
@@ -0,0 +1,13 @@
+package com.cribl.ydorego.logcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogcollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogcollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cribl/ydorego/logcollection/LogcollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cribl.ydorego.logcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogcollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
